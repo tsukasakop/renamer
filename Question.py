8a4e54e2c371af5f1e2c2a0977e06e26de215331
tkskp_sub <tkskp_sub@outlook.jp>
2021-08-28 08:24:45 +0900

    divide functions

---
+++ Question.py
@@ -0,0 +1,41 @@
+sample_nests = {
+    "var1": {
+        "ans1": {
+            "var1-1": None
+            },
+        "ans2": {
+            "var1-2": None
+            },
+    },
+    "var2": None
+}
+
+
+sample_questions = {
+    "var1": {"options": ["ans1", "ans2"], "option_type": "str"},
+    "var1-1": {"oprion": ["y", "n"]}
+}
+
+# sample = {
+#     "Variable1": {
+#         "question": "Q1",
+#         "option": ["a1","a2"],
+#         "option_type": "str",
+#         "nest": {
+#             "Variable1-1": {
+#                 "question": "Q1-1",
+#                 "option":["y","n"]
+#                 "option_type": "bool"
+#                 "nest": },
+#             "Variable1-2": {
+#                 "question": "Q1-2",
+#                 "option_type": "str",
+#                 "Q1-2": "str"}
+#     }
+# class QTree:
+#     def __init__(self, ):
+        # 辞書から生成
+        
+        # sqlから生成
+        
+        # 変数保存するやつから生成
--- renamer.py
+++ renamer.py
@@ -64,50 +64,46 @@ class Directory:
     for child in self.children:
       child.show_all()
 
-  def rename(self, prefex, joint):
+  def rename(self, flags):
     n = 0
     overlaps = set()
-    for f in natsorted(os.listdir(self.path)):
-      if f in overlaps:
-        current_path = os.path.join(self.path, f+"_tmp")
+    for current_name in natsorted(os.listdir(self.path)):
+      if current_name in overlaps:
+        current_path = os.path.join(self.path, current_name+"_tmp")
       else:
-        current_path = os.path.join(self.path, f)
+        current_path = os.path.join(self.path, current_name)
       if os.path.isfile(current_path):
+        print(f"  {current_name} -> ",end="")
         # newname
-        # self.give_name(zahl=n, flags=)
-        new_name = str(n)
-        if prefex:
-          new_name = new_name + joint + str(self)
-        else:
-          new_name = str(self) + joint + new_name
-        if f in overlaps:
-          new_name = new_name.removesuffix("_tmp")
+        new_name = self.give_name(n, flags)
         new_path = os.path.join(self.path, new_name)
-        if os.path.exists(new_path) and new_path != current_path:
+        # if already new name
+        if new_path == current_path:
+          print("--")
+          n += 1
+          continue
+        if os.path.exists(new_path):
           os.rename(new_path, new_path + "_tmp")
           overlaps.add(new_name)
-        print("  ",end="")
-        print(f, end=" ")
-        print("->", end=" ")
         os.rename(current_path, new_path)
         print(new_name)
         if n > self.file_count:
           break
         n += 1
           
-  def give_name(self, zahl, flags):
-    name = str(zahl)
+  def give_name(self, n, flags):
+    name = str(n)
     if flags["prefex"]:
       name = name + flags["joint"] + str(self)
     else:
       name = str(self) + flags["joint"] + name
     return name
-    
-  def rename_all(self,prefex=False, joint=""):
+      
+  def rename_all(self, flags):
     print(self.path)
-    self.rename(prefex=prefex, joint=joint)
+    self.rename(flags)
     for child in self.children:
-      child.rename_all(prefex=prefex, joint=joint)
+      child.rename_all(flags)
   
   
 
@@ -124,50 +120,28 @@ def main():
       if os.path.isdir(path):
         break
       print("not exists")
-
   else:
     path = sys.argv[1]
   
+  # load directory
   directory = Directory(path)
   directory.show_all()
-  
+
   # read/ask option
   config_dir_path = os.path.join(os.path.dirname(__file__), "config")
-  old_flags = dict()
-  if len(sys.argv) >= 3:
-    config_path = os.path.join(config_dir_path, sys.argv[2])
-    if not os.path.exists(config_path):
-      raise FileNotFoundError("invalid config file")
-    with open(config_path, "r") as f:
-      reader = csv.reader(f)
-      for row in reader:
-        if not row:
-          continue
-        old_flags[row[0]] = row[1]
+  if len(sys.argv) == 3:
+    config_path = os.path.join(config_dir_path, sys.argv[2])  
+    old_flags = load_flags(config_path)
+  else:
+    old_flags = dict()
+  flags = ask(old_flags)
 
-  for q_type, q_dict in OPTIONS.items():
-    for var, question in q_dict.items():
-      if var in old_flags:
-        flags[var] = satisfy_format(q_type, var, old_flags[var])
-        if not flags[var] is None:
-          continue
-      if q_type == "BOOLEAN":
-        flags[var] = ask_boolean(question)
-        continue
-      if q_type == "SELECTION":
-        flags[var] = ask_selection(question)
-        continue
-      if q_type == "STRING":
-        flags[var] = input(question + " (free): ")
-        continue
-      raise ValueError("This question type is not defined. \nPlease report this to developer.")
-
-  directory.rename_all(flags["prefex"], flags["joint"])
+  directory.rename_all(flags)
 
   # save option
-  if len(sys.argv) >= 3:
+  if len(sys.argv) == 3:
     new_flag_names = set(flags) - set(old_flags)
-    if new_flag_names and ask_boolean("update(y) or make new file(n)?"):
+    if new_flag_names and ask_boolean("update csv(y) or make new csv(n)?"):
       # update 
       with open(config_path, 'a') as f:
         w = csv.writer(f)
@@ -199,6 +173,40 @@ def main():
           w.writerow([key, value])
       print("success!!")
 
+
+def load_flags(config_path):
+  flags = dict()
+  if not os.path.exists(config_path):
+    raise FileNotFoundError("invalid config file")
+  with open(config_path, "r") as f:
+    reader = csv.reader(f)
+    for row in reader:
+      if not row:
+        continue
+      flags[row[0]] = row[1]
+  return flags
+
+def ask(old_flags):
+  flags = dict()
+  for q_type, q_dict in OPTIONS.items():
+    for var, question in q_dict.items():
+      if var in old_flags:
+        flags[var] = satisfy_format(q_type, var, old_flags[var])
+        if not flags[var] is None:
+          continue
+        print(f"{var}: {old_flags[var]} is not supported.")
+      if q_type == "BOOLEAN":
+        flags[var] = ask_boolean(question)
+        continue
+      if q_type == "SELECTION":
+        flags[var] = ask_selection(question)
+        continue
+      if q_type == "STRING":
+        flags[var] = input(question + " (free): ")
+        continue
+      raise ValueError("This question type is not defined. \nPlease report this to developer.")  
+  return flags
+
 def ask_boolean(question):
   while True:
     tmp = input(question + " (y/n): ").upper()
@@ -208,12 +216,13 @@ def ask_boolean(question):
       return False
     print("format: y/n")
 
-
-def is_invalidname(self, str):
-    for char in ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]:
-      if char in name:
-        return False
-    return True
+def ask_selection(question, options):
+  usage = options.join("/")
+  while True:
+    ans = input(question + " (" + usage + "): ")
+    if ans in usage:
+      return ans
+    print("usage:{usage}")
 
 def satisfy_format(q_type, var, ans):
 # 一定の値を適切な形に変換して返す
@@ -232,6 +241,13 @@ def satisfy_format(q_type, var, ans):
       return ans.upper
     return None
   raise ValueError("This question type is not defined. \nPlease report this to developer.")
+
+def is_invalidname(self, str):
+    for char in ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]:
+      if char in str:
+        return False
+    return True
+
   
 
 
